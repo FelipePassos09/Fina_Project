80ea9cc43f30ac7f02fa85091826e7311f7a277e
FelipePassos09 <felipe.passos.09@gmail.com>
2024-05-28 09:15:33 -0300

    Dia 1 - Application Core

---
+++ Fina.Core/Requests/Categories/CreateCategoryRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fina.Core.Requests.Categories;
+
+public class CreateCategoryRequest : Request
+{
+    [Required(ErrorMessage = "Título Inválido")]
+    [MaxLength(80, ErrorMessage = "O Título deve conter até 80 caracteres.")]
+    [MinLength(5, ErrorMessage = "O título de ve conter o mínimo de 5 caracteres.")]
+    public string Title { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Descrição inválida.")]
+    public string Description { get; set; } = string.Empty;
+
+}
---
+++ Fina.Core/Requests/Transaction/GetTransactionsByPeriodRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Fina.Core.Requests.Transaction;
+
+public class GetTransactionsByPeriodRequest
+{
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+}
